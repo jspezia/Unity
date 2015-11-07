d07e36a4f7503e5c2c695af40fd989812a9c0c16
Jean SPEZIA <jspezia@e1r8p3.42.fr>
2015-11-07 21:12:08 +0100

    BULLET!

--- Rush/MIAMI/Assets/Scripts/bullet.cs
+++ Rush/MIAMI/Assets/Scripts/bullet.cs
@@ -4,24 +4,29 @@ using System.Collections;
 public class bullet : MonoBehaviour {
 
 	private Vector3		target;
+	private Vector3		direction;
 	public float		speed = 30f;
 	bool				fired;
-	// private Rigidbody2D rb;
 
 	void Start () {
 		fired = false;
-		// rb = (Rigidbody2D)GetComponent<Rigidbody2D>();
 	}
 	
 	void Update () {
 		if (fired) {
+			if (transform.position == target) {
+					target += direction;
+			}
 			transform.position = Vector3.MoveTowards(transform.position, target, Time.deltaTime * speed);
 		}
 	}
 
 public	void Fire (Vector3 _position, Vector3 _target) {
 		fired = true;
+		_target.z = 0f;
 		target = _target;
+		_position.z = 0f;
 		transform.position = _position;
+		direction =  target - transform.position;
 	}
 }
