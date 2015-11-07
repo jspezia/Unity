33f0827ebd42bf72783f2d3fdcdc78d33c03bb7d
Jean SPEZIA <jspezia@e1r8p3.42.fr>
2015-11-07 13:23:42 +0100

    p

--- Rush/MIAMI/Assets/Scripts/movements.cs
+++ Rush/MIAMI/Assets/Scripts/movements.cs
@@ -4,7 +4,10 @@ using System.Collections;
 public class movements : MonoBehaviour {
 
 	public float	maxSpeed = 3f;
+	public float	speed_rotation = 3f;
 	private Rigidbody2D rb;
+
+
 
 
 
@@ -17,5 +20,8 @@ public class movements : MonoBehaviour {
 		float vert = Input.GetAxis("Vertical");
 		rb.velocity = new Vector2(vert * maxSpeed, rb.velocity.y);
 		rb.velocity = new Vector2(hori * maxSpeed, rb.velocity.x);
+
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 0));
+        transform.rotation = Quaternion.LookRotation(Vector3.forward, transform.position - mousePosition);
 	}
 }
