f48f535f03967155f022d652d271d643cec2ccd9
Jean SPEZIA <jspezia@e1r8p3.42.fr>
2015-11-07 12:30:57 +0100

    rush

---
+++ Rush/MIAMI/Assets/Scripts/movements.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movements : MonoBehaviour {
+
+	public float	maxSpeed = 3f;
+	private Rigidbody2D rb;
+
+
+
+	void Start() {
+		rb = (Rigidbody2D)GetComponent<Rigidbody2D>();
+	}
+
+	void Update() {
+		float hori = Input.GetAxis("Horizontal");
+		float vert = Input.GetAxis("Vertical");
+		rb.velocity = new Vector2(vert * maxSpeed, rb.velocity.y);
+		rb.velocity = new Vector2(hori * maxSpeed, rb.velocity.x);
+	}
+}
