8a14322470f53b36a04a64f875dbf6ee51e0db0b
Jean SPEZIA <jspezia@e1r8p3.42.fr>
2015-11-07 15:05:09 +0100

    p

---
+++ Rush/MIAMI/Assets/Scripts/menuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public GameObject PauseUI;
+
+	void Start() {
+	}
+
+	void Update() {
+	}
+
+	public void Start_Game
+	() {
+		Application.LoadLevel(1);
+	}
+
+	public void Exit() {
+		Application.Quit();
+	}
+}
