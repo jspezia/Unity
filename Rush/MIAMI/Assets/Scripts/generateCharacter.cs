93117074a2142b8a0b1a56402862fc133e42feb5
Leo LAPILLONNE <llapillo@e1r8p5.42.fr>
2015-11-07 22:01:45 +0100

    generation characters

---
+++ Rush/MIAMI/Assets/Scripts/generateCharacter.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class generateCharacter : MonoBehaviour {
+	
+	public GameObject	head;
+	public Sprite[]		heads;
+	public GameObject	body;
+	public Sprite[]		bodies;
+	
+	private SpriteRenderer	sRenderer;
+
+	// Use this for initialization
+	void Start () {
+		sRenderer = head.GetComponent<SpriteRenderer> ();
+		sRenderer.sprite = heads[Random.Range (0, 14)];
+		sRenderer = body.GetComponent<SpriteRenderer> ();
+		sRenderer.sprite = bodies[Random.Range (0, 3)];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Rush/MIAMI/Assets/Scripts/player.cs
+++ Rush/MIAMI/Assets/Scripts/player.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
+
 	// Use this for initialization
 	void Start () {
 	
