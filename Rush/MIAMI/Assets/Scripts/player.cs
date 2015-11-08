ec351fccaaa8ef598773ec76bbb84f4a04d927d0
Jean SPEZIA <jspezia@e1r8p3.42.fr>
2015-11-08 13:13:42 +0100

    p

--- Rush/MIAMI/Assets/Scripts/player.cs
+++ Rush/MIAMI/Assets/Scripts/player.cs
@@ -3,6 +3,12 @@ using System.Collections;
 
 public class player : MonoBehaviour {
 
+	void OnCollisionEnter2D(Collision2D coll) {
+		if (coll.gameObject.tag == "bullet_ennemy" || coll.gameObject.tag == "Ennemy") {
+			Debug.Log("Dead Player");
+			Application.LoadLevel(Application.loadedLevel);
+		}
+	}
 
 	// Use this for initialization
 	void Start () {
