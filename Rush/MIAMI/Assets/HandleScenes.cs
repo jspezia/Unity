858e08f81c73d9afa80309255216c164ddd855c6
Lise BRIANT <lbriant@e1r8p4.42.fr>
2015-11-08 09:12:26 +0100

    Ajout HandleScprit

---
+++ Rush/MIAMI/Assets/HandleScenes.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandleScenes : MonoBehaviour {
+	public int levelToLoad;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		Application.LoadLevel(levelToLoad);
+	}
+}
